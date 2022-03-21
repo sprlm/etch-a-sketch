93546e1a389a4ce3e806f2161b0fe96a7c297701
Matthew <matthewtoro17@gmail.com>
2022-03-21 12:58:59 +0800

    Add prompt for setting new grid size on reset

--- script.js
+++ script.js
@@ -22,15 +22,41 @@ function createGrid(container, gridSize) {
     }
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+function getNewGridSize() {
+    let newSize;
+
+    do {
+        newSize = prompt('Please enter the new grid size (1-100):');
+
+        if (newSize === null) {
+            return gridSize;
+        }
+
+    } while (!/^-?\d+$/.test(newSize) || (newSize < 1 || newSize > 100));
+
+    return newSize;
+}
+
 function resetGrid() {
     const gridSquares = document.querySelectorAll('.grid-square');
 
     gridSquares.forEach((gridSquare) => {
         gridSquare.classList.remove('hover');
     });
+
+    removeAllChildNodes(container);
+    gridSize = getNewGridSize();
+    createGrid(container, gridSize);
 }
 
-let gridSize = 16;
+let gridSize = 30;
 const container = document.querySelector('#container');
 const resetButton = document.querySelector('#reset-button');
 
